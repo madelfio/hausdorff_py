e879c856ed27aed3c5b78b7ecb904194999ba4f7
madelfio <marco.adelfio@gmail.com>
2011-06-01 17:34:27 -0400

    Updated search_test.py to use mhausdorff

--- search_test.py
+++ search_test.py
@@ -143,17 +143,11 @@ def CheckResults(list1, list2, list3):
             return False
     return True
 
-def compute_hausdorff(pt_set1, pt_set2, lb_mode, one_way=False):
-    if one_way:
-        return pt_set1.hausdorff(pt_set2, lb_mode)
-    return max(pt_set1.hausdorff(pt_set2, lb_mode),
-               pt_set2.hausdorff(pt_set1, lb_mode))
-
 def compute_hausdorff_by_id(id1, id2, lb_mode, one_way=True):
     if one_way:
-        return index_list[id1][1].hausdorff(index_list[id2][1], lb_mode)
-    return max(index_list[id1][1].hausdorff(index_list[id2][1], lb_mode),
-               index_list[id2][1].hausdorff(index_list[id1][1], lb_mode))
+        return index_list[id1][1].mhausdorff(index_list[id2][1], lb_mode)
+    return max(index_list[id1][1].mhausdorff(index_list[id2][1], lb_mode),
+               index_list[id2][1].mhausdorff(index_list[id1][1], lb_mode))
 
 def init_priority_queue(query_id, lb_mode):
     pq = []
@@ -301,10 +295,10 @@ def main():
     min_mbr_cnt = 20
     max_mbr_cnt = 261
 
-    num_runs = 200 #len(index_list)
+    num_runs = 10 #len(index_list)
     queryList = []
 
-    randseed = hash(datetime.datetime.now())
+    randseed = -2934038849772416773#hash(datetime.datetime.now())
     print "randseed = ", randseed
 
     random.seed(randseed)
